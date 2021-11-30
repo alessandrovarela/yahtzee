575682fd8725a896cd9747180aa825e6530b31a5
Lucas Mendes <lucasrbarrosm@gmail.com>
2021-11-29 19:45:38 -0500

    add struct jet and creation of gitignore

---
+++ yahtzee.c
@@ -0,0 +1,30 @@
+// ****************************************************************************
+// 
+// File name : yahtzee.c
+//
+// Author : Lucas
+// 
+// Creation date : 29/11/2021
+//
+// 
+//
+// ****************************************************************************
+
+#include <stdio.h>
+#include <stdio.h>
+
+ #define NUMDES 5
+
+typedef struct Jet Jet;
+struct Jet
+{
+    int jet[NUMDES];
+};
+
+
+
+int main()
+{
+ printf("inicio");
+
+}
